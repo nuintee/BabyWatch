6a173ac0412fd20a6c98acdf7f2c60b257a36145
staff <staff@iMacA.local>
2021-02-12 18:55:01 +0900

    added table

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
 const express = require('express');
-const ss = require('./index.js')
-
+const mongodb = require('mongodb')
+//const ss = require('./index.js')
+//const mdb = require('./db.js')
 const app = express();
+
+const MongoClient = mongodb.MongoClient
+const url = 'mongodb+srv://nuintee:0117@clusterbabywatcher.lgsjj.mongodb.net'
+const dbName = 'BabyChecks'
+
+const client = new MongoClient(url)
 
 app.set("view engine","ejs")
 
-app.get('/', function(req,res){
-    let resu = ss.dataLatest();
-    let param = {
-        resu: resu
-    };
-    res.render("landing",param);
+app.all('/', function(req,res){
+    const db = client.db(dbName)
+    const collection = db.collection('Time');
+    collection.find({}).toArray(function(err, doc){
+        res.render("landing",{data : doc})
+    })
 })
 
 app.all('/api/:who/:when/:what/:how', function(req,res){
-    ss.getSheetRequest(
-        req.params.who,
-        req.params.when,
-        req.params.what,
-        req.params.how
-    );
-    res.json({
-        message:"Hello,world"
+    const who = req.params.who
+    const when = req.params.when
+    const what = req.params.what
+    const how = req.params.how
+    const db = client.db(dbName)
+    const collection = db.collection('Time');
+    collection.insertOne({
+        who,
+        when,
+        what,
+        how
+    }).then(reason => {
+        res.redirect("/");
     });
 })
 
-app.listen(3000, () => console.log('Listening On 3000'))
+client.connect(function(err){
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log('connected to mongodb')
+        app.listen(3000, () => console.log('Listening On 3000'))
+    }
+})
+
+
 

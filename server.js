95dcad9b94cb2b5e468e5fa706fe67145b26b48d
staff <staff@iMacA.local>
2021-02-13 00:37:52 +0900

    done with main feature

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongodb = require('mongodb')
 //const ss = require('./index.js')
 //const mdb = require('./db.js')
 const app = express();
+const bodyParser = require('body-parser');
 
 const MongoClient = mongodb.MongoClient
 const url = 'mongodb+srv://nuintee:0117@clusterbabywatcher.lgsjj.mongodb.net'
@@ -10,6 +11,8 @@ const dbName = 'BabyChecks'
 
 const client = new MongoClient(url)
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set("view engine","ejs")
 
 app.all('/', function(req,res){
@@ -20,20 +23,29 @@ app.all('/', function(req,res){
     })
 })
 
-app.all('/api/:who/:when/:what/:how', function(req,res){
-    const who = req.params.who
-    const when = req.params.when
-    const what = req.params.what
-    const how = req.params.how
+app.post('/api',function(req,res){
+    //Date
+    let now = new Date();
+    let month = now.getMonth();
+    let date = now.getDate();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let time = `${month} / ${date} ${hour}:${minutes}`
+
+    //console.log(req)
+    const who = req.body.who;
+    const what = req.body.what;
+    const when = time;
+    const portion = req.body.portion;
     const db = client.db(dbName)
     const collection = db.collection('Time');
     collection.insertOne({
         who,
         when,
         what,
-        how
+        portion
     }).then(reason => {
-        res.redirect("/");
+        res.redirect('/')
     });
 })
 
